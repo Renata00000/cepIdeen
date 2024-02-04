0ae5bfb6a2033ce5c2a8ca7fba58a84ef66bc5a6
Renata <renata0cristiane@gmail.com>
2024-02-04 14:55:52 -0300

    comentando class de exception

--- src/main/java/br/com/ideencep/ideenCep/excepition/TratandoErros.java
+++ src/main/java/br/com/ideencep/ideenCep/excepition/TratandoErros.java
@@ -1,38 +1,38 @@
-package br.com.ideencep.ideenCep.excepition;
-
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-@RestControllerAdvice
-public class TratandoErros {
-
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity tratarErro404() {
-        return ResponseEntity.notFound().build();
-    }
-
-         @ExceptionHandler(RuntimeException.class)
-        public ResponseEntity tratarErro500() {return ResponseEntity.notFound().build();
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
-        var erros = ex.getFieldErrors();
-        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
-    }
-
-
-    //DTO
-    private record DadosErroValidacao(String campo, String mesagem){
-        public  DadosErroValidacao(FieldError erro){
-            this(erro.getField(), erro.getDefaultMessage());
-        }
-    }
-
-
-}
+//package br.com.ideencep.ideenCep.excepition;
+//
+//import jakarta.persistence.EntityNotFoundException;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.validation.FieldError;
+//import org.springframework.web.bind.MethodArgumentNotValidException;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.RestControllerAdvice;
+//
+//@RestControllerAdvice
+//public class TratandoErros {
+//
+//
+//    @ExceptionHandler(EntityNotFoundException.class)
+//    public ResponseEntity tratarErro404() {
+//        return ResponseEntity.notFound().build();
+//    }
+//
+//         @ExceptionHandler(RuntimeException.class)
+//        public ResponseEntity tratarErro500() {return ResponseEntity.notFound().build();
+//    }
+//
+//    @ExceptionHandler(MethodArgumentNotValidException.class)
+//    public ResponseEntity tratarErro400(MethodArgumentNotValidException ex) {
+//        var erros = ex.getFieldErrors();
+//        return ResponseEntity.badRequest().body(erros.stream().map(DadosErroValidacao::new).toList());
+//    }
+//
+//
+//    //DTO
+//    private record DadosErroValidacao(String campo, String mesagem){
+//        public  DadosErroValidacao(FieldError erro){
+//            this(erro.getField(), erro.getDefaultMessage());
+//        }
+//    }
+//
+//
+//}
